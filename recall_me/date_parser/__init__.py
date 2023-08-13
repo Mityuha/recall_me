192849253e2e96956d73d5501b6310f427a8d86f
mityuha <mit.makaroff@gmail.com>
2023-08-13 20:46:17 +0200

    import name

--- recall_me/date_parser/__init__.py
+++ recall_me/date_parser/__init__.py
@@ -2,3 +2,4 @@ from .digit_date_parser import DigitDateParser
 from .parser import parse
 from .text_day_and_month_date_parser import DayTextDateParser
 from .text_month_date_parser import TextDateParser
+from .utils import MONTH_NUM_2_NAME
